2b703e5717a08b8c9af388bc59f504537bd6ef10
AlexisWilke <alexis@m2osw.com>
2022-06-05 16:08:52 -0700

    Updated the copyright notice to 2022.

--- libutf8/version.cpp
+++ libutf8/version.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2000-2021  Made to Order Software Corp.  All Rights Reserved
+// Copyright (c) 2000-2022  Made to Order Software Corp.  All Rights Reserved
 //
 // https://snapwebsites.org/
 // contact@m2osw.com
