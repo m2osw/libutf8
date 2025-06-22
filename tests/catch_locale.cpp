4c4d95a623aa76f421fd814be6da3e10ea06f8f3
AlexisWilke <alexis@m2osw.com>
2025-06-21 21:29:36 -0700

    Add tests for the locale sets.

---
+++ tests/catch_locale.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2025  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/libutf8
+// contact@m2osw.com
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+// libutf8
+//
+#include    <libutf8/locale.h>
+
+
+// unit test
+//
+#include    "catch_main.h"
+
+
+// C++
+//
+//#include    <cctype>
+//#include    <iostream>
+//#include    <iomanip>
+
+
+// last include
+//
+#include    <snapdev/poison.h>
+
+
+
+CATCH_TEST_CASE("locale_exists", "[locale][valid]")
+{
+    CATCH_START_SECTION("locale_exists: check that the C and POSIX system locales exist")
+    {
+        CATCH_CHECK(libutf8::has_system_locale("C"));
+        CATCH_CHECK(libutf8::has_system_locale(std::string("POSIX")));
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("locale_exists: check that the \"en\" and \"fr\" ICU locales exist")
+    {
+        CATCH_CHECK(libutf8::has_icu_locale("en"));
+        CATCH_CHECK(libutf8::has_icu_locale(std::string("fr")));
+    }
+    CATCH_END_SECTION()
+}
+
+
+// vim: ts=4 sw=4 et
